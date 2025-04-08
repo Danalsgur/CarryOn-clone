dcec3c4924b59e7cb82344db6b91ee158a03b139
Minhyuk Kim <haeunkim@MacBook-Air-3.local>
2025-04-08 09:27:11 +0100

    fix: 작은 수정 반영

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,17 +1,19 @@
-export interface Item {
+export interface Request {
+  id: string;
+  buyer_id: string;
+  buyer_nickname: string;
+  items: {
     name: string;
+    url: string;
+    price: string;
     size: string;
-  }
-  
-  export interface Request {
-    id: number;
-    city: string;
-    start_date: string;
-    end_date: string;
-    status: string;
-    carrier_nickname?: string;
-    items: Item[];
-    reward?: string;
-    chat_link?: string;
-  }
-
+  }[];
+  reward: string;
+  city: string;
+  start_date: string;
+  end_date: string;
+  chat_link: string;
+  notes?: string;
+  status: "대기중" | "매칭완료" | "삭제됨";
+  created_at: string;
+}
